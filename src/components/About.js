1eb8bbc09c650defd1e27c9105cc789d319fbba2
Kimani Muhammad <kimanimuhammad@gmail.com>
2024-07-27 12:36:30 -0700

    Fixed bug in download button

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,23 @@ import React from "react";
 import '../About.css';
 
 const About = () => {
+
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = '../Kimani_Muhammad_Software_Dev_Resume.pdf';
+        link.download = 'Kimani_Muhammad_Software_Dev_Resume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
+
     return (
         <section className="container">     
             <section className="card-border">
                 <section className="card" id="about">
                     <h2>About Me</h2>
                     <p>I am a full stack web developer with a background in education and a passion for learning. I have experience in JavaScript, CSS, and HTML, as well as skills in MongoDB, Express.js, React.js, and Node.js. I am a natural problem solver and enjoy working in a team environment. I am excited to bring my unique background to a developer role.</p>
-                    <button className="download-button" onClick={() => 
-                        window.location.href = "../Kimani_Muhammad_Software_Dev_Resume.pdf"
-                    }>Download My Resume</button>
+                    <button className="download-button" onClick={handleDownload}>Download My Resume</button>
                 </section>
             </section>
         </section>
